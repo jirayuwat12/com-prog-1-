228e229811d6a31a72c23b8236a295cc4ef8202d
jirayuwat12 <49447426+jirayuwat12@users.noreply.github.com>
2021-11-09 19:26:42 +0700

    completed

--- grader/10_NumPy_24.py
+++ grader/10_NumPy_24.py
@@ -2,22 +2,18 @@ import numpy as np
 def peak_indexes(x):
     # x เป็นอาเรย์เก็บจ านวนต่าง ๆ
     # คืนอาเรย์ที่เก็บต าแหน่งใน x ที่เป็น "ยอด"
-    ret = []
-    if x[0] > x[1]:
-        ret.append(0)
-    if x[-2] > x[-1]:
-        ret.append(len(x)-1)
-    i = 1
-    while i < len(x)-1:
-        if x[i-1] < x[i] and x[i] > x[i+1]:
-            ret.append(i)
-        i += 1
-    return ret 
+    m = np.append([99999999999999],x[:-1])
+    n = np.append(x[1:],[99999999999999999])
+    x = (x > m) & (x > n)
+    # print(m,n)
+    return list(np.where(x == True))[0]
+    
 def main():
     d = np.array([float(e) for e in input().split()])
     pos = peak_indexes(np.array(d))
     if len(pos) > 0:
-        print(", ".join([str(e) for e in pos]))
+        print(', '.join([str(e) for e in pos]))
     else:
         print("No peaks")
 exec(input().strip()) # Don't remove this line
+# main()
